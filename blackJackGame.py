ecbd1129be7994195caa3470c2edc3a3c00e0477
Kauã de Camargo <kauacamargo40@gmail.com>
2026-06-16 14:14:16 -0300

    feat: basic game mechanics added

--- blackJackGame.py
+++ blackJackGame.py
@@ -5,13 +5,13 @@ def main():
     if start_game().lower() =="e":
             print("Bye bye!")
     else:
-        deck_shuffled = create_shuffled_deck(1)
-        player_cards, dealer_cards = send_cards(deck_shuffled)
-        player_points, dealer_points = calculate_points(player_cards, dealer_cards)
-        print(player_cards)
-        print(dealer_cards)
-        print(player_points)
-        print(dealer_points)
+        deck = create_shuffled_deck(1)
+        player_cards, dealer_cards = send_cards(deck)
+
+        print("So... let's go play!")
+        time.sleep(1)
+
+        game(deck, player_cards, dealer_cards)
 
 def start_game():
     print("-" * 10)
@@ -65,58 +65,103 @@ def send_cards(deck):
 
     return player_cards, dealer_cards
 
-def calculate_points(player_cards, dealer_cards):
-    print("So... let's go play!")
-    time.sleep(1)
-
+def calculate_points(cards):
     special_cards = ["J", "Q", "K"]
 
-    player_points = 0
-    dealer_points = 0
+    value_of_cards = 0
 
-    for i in range(2):
+    for i in range(len(cards)):
         if i == 0:
-            if player_cards[i][1:] in special_cards:
-                player_points = 10
-            elif player_cards[i][1:] == "A":
-                player_points = 1
+            if cards[i][1:] in special_cards:
+                value_of_cards = 10
+            elif cards[i][1:] == "A":
+                value_of_cards = 1
             else:
-                player_points = int(player_cards[i][1:])
-            
-            if dealer_cards[i][1:] in special_cards:
-                dealer_points = 10
-            elif dealer_cards[i][1:] == "A":
-                dealer_points = 1
-            else:
-                dealer_points = int(dealer_cards[i][1:])
+                value_of_cards = int(cards[i][1:])
         else:
-            if player_cards[i][1:] in special_cards:
-                if player_points == 1:
-                    player_points += 20
+            if cards[i][1:] in special_cards + ["10"]:
+                if value_of_cards == 1:
+                    value_of_cards += 20
                 else:
-                    player_points += 10
-            elif player_cards[i][1:] == "A":
-                if player_points == 10:
-                    player_points += 11
+                    value_of_cards += 10
+            elif cards[i][1:] == "A":
+                if value_of_cards == 10:
+                    value_of_cards += 11
                 else:
-                    player_points += 1
+                    value_of_cards += 1
             else:
-                player_points += int(player_cards[i][1:])
+                value_of_cards += int(cards[i][1:])
 
-            if dealer_cards[i][1:] in special_cards:
-                if dealer_points == 1:
-                    dealer_points += 20
-                else:
-                    dealer_points += 10
-            elif dealer_cards[i][1:] == "A":
-                if dealer_points == 10:
-                    dealer_points += 11
-                else:
-                    dealer_points += 1
+    return value_of_cards
+
+def dealer_round(deck, dealer_points, player_points, dealer_cards):
+    print(f"Dealer cards: {' | '.join(dealer_cards)}")
+    time.sleep(1)
+    
+    end_round = False
+
+    while end_round == False:
+        if dealer_points > 21:
+            print("You win, the dealer busted! The game will be restarted.")
+            end_round = True
+            return end_round
+        elif dealer_points > player_points:
+            print("You lost, dealer won! The game will be restarted.")
+            end_round = True
+            return end_round
+        else:
+            dealer_cards.append(deck.pop())
+            dealer_points = calculate_points(dealer_cards)
+            print(f"New dealer card: {dealer_cards[-1]}")
+            time.sleep(1)
+            continue
+
+def game(deck, player_cards, dealer_cards):
+    
+    player_points = calculate_points(player_cards)
+    dealer_points = calculate_points(dealer_cards)
+
+    if (player_points == 21 and len(player_cards) == 2) and dealer_points != 21:
+        print("Congratulations, you won with a BlackJack! The game will be restarted.")
+    elif (player_points == 21 and len(player_cards) == 2) and (dealer_points == 21 and len(dealer_cards) == 2):
+        print("Draw! You and dealer have Blackjack! The game will be restarted.")
+    elif dealer_points == 21:
+        print("You lost! Dealer has a BlackJack! The game will be restarted.")
+        print(f"Dealer cards: {' | '.join(dealer_cards)}")
+    else:
+         while True:
+            response = input(f"You have {' | '.join(player_cards)}\nType S to stand, H to hint or E to exit\n")
+            if response.lower() == 'e':
+                break
+            elif response.lower() == 's':
+                print(f"Your cards: {' | '.join(player_cards)}")
+
+                if dealer_round(deck, dealer_points, player_points, dealer_cards):
+                    print(f"Dealer cards: {' | '.join(dealer_cards)}")
+                    print(f"Your cards: {' | '.join(player_cards)}")
+                    break
             else:
-                dealer_points += int(dealer_cards[i][1:])
+                print(f"Your cards: {' | '.join(player_cards)}")
+                time.sleep(1)
 
-    return player_points, dealer_points
+                new_player_card = deck.pop()
+                player_cards.append(new_player_card)
+
+                print(f"New card: {player_cards[-1]}")
+                time.sleep(1)
+
+                player_points = calculate_points(player_cards)
+
+                if player_points > 21:
+                    print(f"Your cards: {' | '.join(player_cards)}")
+                    print("You lost! Your points passed 21! The game will be restarted.")
+                    break
+                elif player_points == 21:
+                    print(f"Your cards: {' | '.join(player_cards)}")
+                    print("21! Congratulations, you won! The game will be restarted.")
+                    break
+                else:
+                    continue
 
 if __name__ == "__main__":
     main()
